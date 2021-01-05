6b513325e693c8604fdbd96359440ba9bbc58b65
lxj <425862717@qq.com>
2021-01-05 23:17:30 +0800

    用户注册页面

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <div class="jumbotron">
-        <h1>Hello Laravel</h1>
+        <h1>Hello World</h1>
         <p class="lead">
             你现在所看到的是Lxj的世界
         </p>
@@ -12,7 +12,7 @@
             一切，将从这里开始。
         </p>
         <p>
-            <a class="btn btn-lg btn-success"href="#"role="button">现在注册</a>
+            <a class="btn btn-lg btn-success" href="{{ route('signup')}}" role="button">现在注册</a>
         </p>
     </div>
 @endsection
