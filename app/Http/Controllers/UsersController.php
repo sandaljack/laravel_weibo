796ff2ab410c70142ef1f3070e8d041d4ad86746
lxj <425862717@qq.com>
2021-01-14 18:11:57 +0800

    管理员可删除用户

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -77,6 +77,14 @@ class UsersController extends Controller
 
     }
 
+    public function destroy(User $user)
+    {
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '成功删除用户！');
+        return back();
+    }
+
 
 
 }
