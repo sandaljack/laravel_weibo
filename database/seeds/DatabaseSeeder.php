9fc7f806712c70eb54f125b8772de77fd9db9b7e
lxj <425862717@qq.com>
2021-01-16 16:58:18 +0800

    用户微博列表

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,10 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
+
         $this->call(UsersTableSeeder::class);
+        $this->call(StatusesTableSeeder::class);
+
 
         Model::unguard();
     }
